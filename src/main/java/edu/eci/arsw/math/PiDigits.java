48da349e14b99a7c378a7f29a7cd85a9df4cecb5
unknown <daniel.ruge-g@labinfo.is.escuelaing.edu.co>
2025-09-19 09:06:11 -0500

    variable N de hilos

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -18,13 +18,6 @@ public class PiDigits {
      * @return An array containing the hexadecimal digits.
      */
     public static byte[] getDigits(int start, int count, int N) {
-        for(int i = 0; i<= N; i++){
-            int segmento = count/N;
-            PiDigitsThreads hilo = new PiDigitsThreads(start,segmento);
-            hilo.run();
-    
-
-        }
         
         if (start < 0) {
             throw new RuntimeException("Invalid Interval");
@@ -34,87 +27,18 @@ public class PiDigits {
             throw new RuntimeException("Invalid Interval");
         }
 
-        byte[] digits = new byte[count];
-        double sum = 0;
+        byte[] digits = new byte[0];
 
-        for (int i = 0; i < count; i++) {
-            if (i % DigitsPerSum == 0) {
-                sum = 4 * sum(1, start)
-                        - 2 * sum(4, start)
-                        - sum(5, start)
-                        - sum(6, start);
+        for(int i = 0; i<= N; i++){
+            int segmento = count/N;
+            PiDigitsThreads hilo = new PiDigitsThreads(start,segmento);
+            hilo.run();
 
-                start += DigitsPerSum;
-            }
 
-            sum = 16 * (sum - Math.floor(sum));
-            digits[i] = (byte) sum;
         }
 
         return digits;
     }
 
-    /// <summary>
-    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
-    /// </summary>
-    /// <param name="m"></param>
-    /// <param name="n"></param>
-    /// <returns></returns>
-    private static double sum(int m, int n) {
-        double sum = 0;
-        int d = m;
-        int power = n;
-
-        while (true) {
-            double term;
-
-            if (power > 0) {
-                term = (double) hexExponentModulo(power, d) / d;
-            } else {
-                term = Math.pow(16, power) / d;
-                if (term < Epsilon) {
-                    break;
-                }
-            }
-
-            sum += term;
-            power--;
-            d += 8;
-        }
-
-        return sum;
-    }
-
-    /// <summary>
-    /// Return 16^p mod m.
-    /// </summary>
-    /// <param name="p"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    private static int hexExponentModulo(int p, int m) {
-        int power = 1;
-        while (power * 2 <= p) {
-            power *= 2;
-        }
-
-        int result = 1;
-
-        while (power > 0) {
-            if (p >= power) {
-                result *= 16;
-                result %= m;
-                p -= power;
-            }
-
-            power /= 2;
-
-            if (power > 0) {
-                result *= result;
-                result %= m;
-            }
-        }
-
-        return result;
-    }
 
 }
--- src/main/java/edu/eci/arsw/math/PiDigitsThreads.java
+++ src/main/java/edu/eci/arsw/math/PiDigitsThreads.java
@@ -4,8 +4,10 @@ package edu.eci.arsw.math;
 public class PiDigitsThreads extends Thread {
     private int start;
     private int count;
-    private PiDigits digits;
     private Boolean lock;
+    private byte[] digits;
+    private static int DigitsPerSum = 8;
+    private static double Epsilon = 1e-17;
 
 
 
@@ -16,11 +18,76 @@ public class PiDigitsThreads extends Thread {
 
     @Override
     public void run() {
-        
+        byte[] digits = new byte[count];
+        double sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (i % DigitsPerSum == 0) {
+                sum = 4 * sum(1, start)
+                        - 2 * sum(4, start)
+                        - sum(5, start)
+                        - sum(6, start);
+
+                start += DigitsPerSum;
+            }
+
+            sum = 16 * (sum - Math.floor(sum));
+            digits[i] = (byte) sum;
+        }
         
         
 
+        }
+    
+        private static double sum(int m, int n) {
+            double sum = 0;
+            int d = m;
+            int power = n;
+    
+            while (true) {
+                double term;
+    
+                if (power > 0) {
+                    term = (double) hexExponentModulo(power, d) / d;
+                } else {
+                    term = Math.pow(16, power) / d;
+                    if (term < Epsilon) {
+                        break;
+                    }
+                }
+    
+                sum += term;
+                power--;
+                d += 8;
+            }
+    
+            return sum;
+        }
 
+        private static int hexExponentModulo(int p, int m) {
+            int power = 1;
+            while (power * 2 <= p) {
+                power *= 2;
+            }
+    
+            int result = 1;
+    
+            while (power > 0) {
+                if (p >= power) {
+                    result *= 16;
+                    result %= m;
+                    p -= power;
+                }
+    
+                power /= 2;
+    
+                if (power > 0) {
+                    result *= result;
+                    result %= m;
+                }
+            }
+    
+            return result;
         }
         
         
